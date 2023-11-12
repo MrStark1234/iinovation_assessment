187665b9fcd10899909e805350cb33af06c60623
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2023-11-12 11:08:32 +0530

    resolve json error

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ const PORT = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGOURL)
   .then(() => console.log("Connected to Database"));
